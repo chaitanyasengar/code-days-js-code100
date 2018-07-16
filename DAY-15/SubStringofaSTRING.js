475546292e9989705dd3adde415578948586f043
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-07-16 23:57:31 +0530

    Add files via upload

---
+++ DAY-15/SubStringofaSTRING.js
@@ -0,0 +1,23 @@
+function subStringFinder(str, subStr){
+  var idx = 0,
+      i = 0,
+      j = 0,
+      len = str.length,
+      subLen = subStr.length;
+
+   for(; i<len; i++){
+   
+      if(str[i] == subStr[j])
+         j++;
+      else
+         j = 0;
+      
+        
+      if(j == 0)
+        idx = i;
+      else if (j == subLen)
+        return idx;
+  }
+
+  return -1;
+}
