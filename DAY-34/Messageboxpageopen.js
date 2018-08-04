5878de2541bda18afdba579668419d674b953b50
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-08-04 23:32:00 +0530

    Messagebox.js

---
+++ DAY-34/Messageboxpageopen.js
@@ -0,0 +1,9 @@
+// javascript  appear a message box at the time page is opened. 
+<html>at-->
+<head>
+<script language="javascript" type="text/javascript">
+alert("Welcome to my site")
+</script>
+</head>
+</html>       
+<font face="Tahoma"><a target="_blank" href="http://www.kiet group of institutions.com/"><span style="font-size: 8pt; text-decoration: none">JavaScript Free Code</span></a></font>
