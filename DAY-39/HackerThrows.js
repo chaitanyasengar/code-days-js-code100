6b4192ae5402f3e1742404e758782bff3b7904dc
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-08-09 22:46:28 +0530

    HackerThrows

---
+++ DAY-39/HackerThrows.js
@@ -0,0 +1,18 @@
+
+// Throws
+
+function isPositive(a) {
+     if (a > 0)
+        {
+            return "YES";
+        }
+    else if (a === 0)
+        {
+            throw new Error("Zero Error");
+        }
+    else if (a < 0)
+        {
+            throw new Error("Negative Error");
+        }
+    
+}
