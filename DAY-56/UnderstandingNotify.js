c7395147f1b65f48b1b5449b7b3d1da6d900d3fb
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-08-26 19:57:59 +0530

    UnderstandingNotify

---
+++ DAY-56/UnderstandingNotify.js
@@ -0,0 +1,17 @@
+
+//Understand  notifyAll
+<script>   
+   function notifyAll(fnSms, fnEmail) {   
+      console.log('starting notification process');   
+      fnSms();   
+      fnEmail();   
+   }   
+   notifyAll(function() {   
+      console.log("Sms send ..");   
+   }, 
+   function() {   
+      console.log("email send ..");   
+   });   
+   console.log("End of script"); 
+   //executes last or blocked by other methods   
+</script> 
