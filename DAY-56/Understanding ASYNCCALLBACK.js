358e0eea060e34c89db3749f14d19d71d31c933a
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-08-26 20:00:42 +0530

    Understanding ASYNC CALLBACK

---
+++ DAY-56/Understanding ASYNCCALLBACK.js
@@ -0,0 +1,18 @@
+// Understanding ASYNC CALLBACK
+
+<script>   
+   function notifyAll(fnSms, fnEmail) {   
+      setTimeout(function() {   
+         console.log('starting notification process');   
+         fnSms();   
+         fnEmail();   
+      }, 2000);   
+   }   
+   notifyAll(function() {   
+      console.log("Sms send ..");   
+   },  
+   function() {   
+      console.log("email send ..");   
+   });   
+   console.log("End of script"); //executes first or not blocked by others   
+</script>
