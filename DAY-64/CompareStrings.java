7dbbea2be9ae9da5b28d6cd54a74f840a96ed2b3
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-09-04 23:55:21 +0530

    ComapreStrings

---
+++ DAY-64/CompareStrings.java
@@ -0,0 +1,15 @@
+//Compare Strings
+
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String style = new String("Bold");
+        String style2 = new String("Bold");
+
+        if(style.equals(style2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
