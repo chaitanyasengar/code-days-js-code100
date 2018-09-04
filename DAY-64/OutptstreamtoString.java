6cc785e36805e6fd912de2754e98f53a8616d757
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-09-04 23:51:01 +0530

    OutputStreamtoString

---
+++ DAY-64/OutptstreamtoString.java
@@ -0,0 +1,16 @@
+//OUTPUT stream to STRING
+import java.io.*;
+
+public class OutputStreamString {
+
+    public static void main(String[] args) throws IOException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String line = "Hello there!";
+
+        stream.write(line.getBytes());
+        String finalString = new String(stream.toByteArray());
+
+        System.out.println(finalString);
+    }
+}
