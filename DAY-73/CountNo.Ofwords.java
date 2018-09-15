98d5f342657fc230f41f4b8b355b0bd3b8442ff8
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-09-15 21:15:12 +0530

    CountNoOfWords

---
+++ DAY-73/CountNo.Ofwords.java
@@ -0,0 +1,25 @@
+//Method to count no. of words in provided String
+
+public class CountWordsInString {
+    
+    static int countWords(String inputString){
+        String[] strarray = inputString.split(" ");  // Spilt String by Space
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+        for(String s:strarray){
+            if(!s.equals("")){
+               count++;
+            }    
+        }
+        return  count;
+        
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String str = sc.nextLine();
+        System.out.println("No. of Words in String : "+countWords(str));
+    }
+    }
+	
