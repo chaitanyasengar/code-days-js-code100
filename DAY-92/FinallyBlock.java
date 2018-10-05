5c0f9315a11b2eae19ee6219d0974459a0d380f2
chaitanyasengar <35967714+chaitanyasengar@users.noreply.github.com>
2018-10-05 20:35:23 +0530

    FinallyBlock

---
+++ DAY-92/FinallyBlock.java
@@ -0,0 +1,18 @@
+ c
+ //Finally Block
+ 
+ lass Allocate {
+  public static void main(String[] args) {
+   
+    try {
+      long data[] = new long[1000000000];
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+   
+    finally {
+      System.out.println("finally block will execute always.");
+    }
+  }
+}
